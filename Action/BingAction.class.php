b65b337b8c4d2a2823fe4c64045eb0a000c45db1
hlydream <1605104939@qq.com>
2016-05-18 19:55:28 +0800

    1.2
    修改了必应壁纸的一些问题。
    修改了当用户没有指定宽 和高的参数的时候，自动获取浏览器宽高而不是默认的1920X1080。

--- Action/BingAction.class.php
+++ Action/BingAction.class.php
@@ -1,24 +1,56 @@
 <?php
 	class BingAction extends Action{
+		#定义两个类成员变量  必应壁纸的api  
 		protected $url_left ='http://cn.bing.com/HPImageArchive.aspx?format=js&idx=';
 		protected $url_right ='&n=1';
 		public function GetBing(){
+			#这个参数是获取今天壁纸还是前几天的默认为0今天
 			$data = defined('DATA') ? DATA : 0;
 			$url = $this->url_left.$data.$this->url_right;
-			$width = defined('W')? W : 1920;
-			$height = defined('H')? H : 1080;
 			$json = file_get_contents($url);
 			$res=json_decode($json, true);
+			$title = $res['images']['0']['msg']['0']['text'];
 			$url=$res['images']['0']['url'];
-			$url=substr($url,0,strrpos($url,'_'))."_$width".'x'.$height.".jpg";
+			#如果发现定义了W H常量代表URL传参过来了w h的值 那么开始构造壁纸的url
+			if(defined('W')||defined('H')){
+				$width=W;
+				$height=H;
+				$url=substr($url,0,strrpos($url,'_'))."_$width".'x'.$height.".jpg";
+				#因为获取到了用户传入的宽 高 那么 $js $body_js两个参数就为空，防止运行自动获取屏幕的宽高
+				$js='';
+				$body_js='';
+			}else{
+				$url=substr($url,0,strrpos($url,'_'))."_";
+				$body_js = "imgUrl()";
+				#如果没有url没有传壁纸的高 宽参数就创建变量 $js 来获取到当前用户的分辨率 并且修改img标签里面的src地址
+$js=<<<js
+	<script>
+		var url="$url";
+		function imgUrl(){
+			var image=document.getElementById("image"); 
+            image.setAttribute("src",url+screen.width+'x'+screen.height+".jpg"); 
+		}
+	</script>
+js;
+			}
 $html=<<<html
 	<html>
 	<head>
 	<meta name="viewport" content="width=device-width,height=device-height, minimum-scale=0.1">
-	<title>Bing</title>
-	</head>
-	<body style="margin: 0px;">
-		<img id="img" style="-webkit-user-select: none; cursor: zoom-in;" src="$url" width="auto" height="auto">
+	<title>$title</title>
+<style type="text/css">
+ body {
+	background-image: url(http://www.lzi520.com/Public/sdshare/images/bg.png);
+	background-repeat: repeat;
+	background-color: #9cd9f2;
+}
+</style>
+$js
+</head>
+	<body onload="$body_js">
+		<div>
+			 <img id="image" src="$url">
+		</div>
 	</body>
 	</html>
 html;
